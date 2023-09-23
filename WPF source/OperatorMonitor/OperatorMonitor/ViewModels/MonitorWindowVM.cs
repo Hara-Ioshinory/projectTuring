61fcccefddbf3eb206ec59c2e57ed4c94b4d941c
Hara-Ioshinory <hara.ioshinory@gmail.com>
2023-09-23 16:24:10 +0300

    Начало файловой системы

--- WPF source/OperatorMonitor/OperatorMonitor/ViewModels/MonitorWindowVM.cs
+++ WPF source/OperatorMonitor/OperatorMonitor/ViewModels/MonitorWindowVM.cs
@@ -1,13 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using OperatorMonitor.Interstructures;
+using System.Windows.Input;
+using OperatorMonitor.Models;
+using System.Diagnostics;
 
 namespace OperatorMonitor.ViewModels
 {
     internal class MonitorWindowVM: ViewModel
     {
+        VideoClumpModel VideoModel = new();
+        public ICommand OpenFileCMD { get; }
+        bool CanOpenFile(object p) => true;
+        void OnOpenFile(object o)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "Выбирете видеофайлы";
+            op.Multiselect = true;
+            op.Filter = "Видео|*.mp4";
+            if (op.ShowDialog() == true)
+            {
+                foreach(string fileName in op.FileNames)
+                {
+                    Trace.WriteLine($"Файл: {fileName}");
+                }
+            }
+        }
 
+        public MonitorWindowVM()
+        {
+            OpenFileCMD = new LambdaCommand(OnOpenFile, CanOpenFile);
+        }
     }
 }
