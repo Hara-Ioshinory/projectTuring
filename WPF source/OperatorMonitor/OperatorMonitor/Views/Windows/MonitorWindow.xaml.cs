8204b16bb7afac11ed013bf716223464147c79d0
Hara-Ioshinory <hara.ioshinory@gmail.com>
2023-09-23 11:21:35 +0300

    Ресурсы
    Добавлены цвета и кисти, подключен словарь

---
+++ WPF source/OperatorMonitor/OperatorMonitor/Views/Windows/MonitorWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace OperatorMonitor
+{
+    public partial class MonitorWindow : Window
+    {
+        public MonitorWindow()
+        {
+            InitializeComponent();
+            MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+            MaxWidth = SystemParameters.MaximizedPrimaryScreenWidth;
+        }
+    }
+}
